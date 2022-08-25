3c8bb288c37d7dede9f129d8d30016e166d64f3c
samcarson <carsonsam4@gmail.com>
2022-08-25 14:55:31 -0700

    proxy set up, useEffect recieves data

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,19 @@ function App() {
     const access_token = urlParams.get("access_token");
     const refresh_token = urlParams.get("refresh_token");
 
-    console.log(querystring);
     console.log("ACCESS TOKEN");
     console.log(access_token);
     console.log("REFRESH TOKEN");
     console.log(refresh_token);
+
+    if (refresh_token) {
+      fetch(`/refresh_token?refresh_token=${refresh_token}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.error(err));
+    }
   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
